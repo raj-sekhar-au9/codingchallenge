fb2353a1bed2db512ad4f25f177855ab3b960928
Raj Sekhar <krishnasylvester@gmail.com>
2020-07-10 11:38:10 +0530

    Update cod_week2_day2.py

--- cod_week2_day2.py
+++ cod_week2_day2.py
@@ -1,4 +1,12 @@
 # input a number 
 print ("enter a number")
 for x in range(int(input())):
-   print (f"the answer is  : {x**2}")      
+   print (f"the answer is  : {x**2}")  
+   
+   #the other coding assignment is in 
+ 
+# https://github.com/rajsekharau9/codingchallenge/blob/master/cod-week-02%20day-4.py
+
+#assiignment is in 
+# https://github.com/rajsekharau9/assignments
+#sorry for late submisssion as it is new to me and am not completely clear regardind the functioning
